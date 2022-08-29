c5d73a4c78bdbe3eceebfa65fa0c51c18b75a457
ailec alviarez <alviarezailec@gmail.com>
2022-08-29 14:09:19 -0400

    anadiendo carrusel a services

--- main.js
+++ main.js
@@ -16,16 +16,33 @@
 //
 //     });
 autoplay:false
-autoplayTimeout:5000
+autoplayTimeout:3000
 autoplayHoverPause:false
+
 
 $(document).ready(function(){
     var owl = $(".owl-carousel");
     owl.owlCarousel({
-        items: 1,
+        items: 2,
         autoplay: true,
         autoPlaySpeed: 5000,
         autoPlayTimeout: 5000,
-        autoplayHoverPause: true
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: true
+            },
+            600:{
+                items:2,
+                nav: false
+            },
+            1000:{
+                items:2,
+                nav:true,
+                loop: false
+            }
+        }
     });
 });
