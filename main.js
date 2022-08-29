0adebb50b6f588630e883063e2e373b75da228f8
ailec alviarez <alviarezailec@gmail.com>
2022-08-29 16:40:01 -0400

    arreglo de carousel service

--- main.js
+++ main.js
@@ -8,23 +8,24 @@ autoplayHoverPause:false
 $(document).ready(function(){
     var owl = $(".owl-carousel");
     owl.owlCarousel({
-        items: 2,
+        items: 1,
         autoplay: true,
         autoPlaySpeed: 5000,
         autoPlayTimeout: 5000,
         autoplayHoverPause: true,
         responsiveClass: true,
+        video:true,
         responsive: {
             0:{
                 items: 1,
                 nav: true
             },
             600:{
-                items:2,
+                items:1,
                 nav: false
             },
             1000:{
-                items:2,
+                items:1,
                 nav:true,
                 loop: false
             }
